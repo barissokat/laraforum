0ab2d6f05391839bda662cd7a3c7c4c73de289fc
barissokat <barissokat@outlook.com>
2020-04-27 04:43:57 +0300

    Spam class is cleaned

--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Unit;
 
-use App\Spam;
+use App\Inspections\Spam;
 use Tests\TestCase;
 
 class SpamTest extends TestCase
@@ -13,10 +13,28 @@ class SpamTest extends TestCase
      *
      * @return void
      */
-    public function itValidatesSpam()
+    public function itChecksForInvalidKeywords()
     {
         $spam = new Spam();
 
         $this->assertFalse($spam->detect('Innocent reply here'));
+
+        $this->expectException(\Exception::class);
+
+        $spam->detect('yahoo customer support');
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function itChecksForAnyKeyBeingHeldDown()
+    {
+        $spam = new Spam();
+
+        $this->expectException(\Exception::class);
+
+        $spam->detect('Hello world aaaaaaaaaaaa');
     }
 }
