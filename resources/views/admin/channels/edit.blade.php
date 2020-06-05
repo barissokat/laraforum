cd09fb19c742b3cbe3d5cd9ed1cc7015857a2b84
Barış Sokat <barissokat@outlook.com>
2020-06-05 09:18:35 +0300

    Fix undefined variable channel in channels admin form partial

--- resources/views/admin/channels/edit.blade.php
+++ resources/views/admin/channels/edit.blade.php
@@ -1,5 +1,8 @@
 @extends('admin.layout.app')
 
 @section('administration-content')
-@include ('admin.channels._form')
+<form method="POST" action="{{ route('admin.channels.update', $channel) }}">
+    {{ method_field('PATCH') }}
+    @include ('admin.channels._form')
+</form>
 @endsection
