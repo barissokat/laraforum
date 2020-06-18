f05c44021008111b302bd07dc1885dcfe700245b
Barış Sokat <barissokat@outlook.com>
2020-06-18 22:12:45 +0300

    Removing unnecessary appends property on Thread; causes N+1 on threads index

--- app/Thread.php
+++ app/Thread.php
@@ -29,13 +29,6 @@ class Thread extends Model
     protected $with = ['owner', 'channel'];
 
     /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array
-     */
-    protected $appends = ['isSubscribedTo'];
-
-    /**
      * The attributes that should be cast to native types.
      *
      * @var array
