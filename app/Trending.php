1bdc0fe32a47eed65ebb39b63b4226090865fd2e
Barış Sokat <barissokat@outlook.com>
2020-06-23 15:14:28 +0300

    Refactor trending to use generic cache driver

--- app/Trending.php
+++ app/Trending.php
@@ -2,20 +2,10 @@
 
 namespace App;
 
-use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
 
 class Trending
 {
-    /**
-     * Get the cache key name.
-     *
-     * @return string
-     */
-    public function cacheKey()
-    {
-        return app()->environment('testing') ? 'testing_trending_threads' : 'trending_threads';
-    }
-
     /**
      * Fetch all trending threads.
      *
@@ -23,7 +13,10 @@ class Trending
      */
     public function get()
     {
-        return array_map('json_decode', Redis::zrevrange($this->cacheKey(), 0, 4));
+        return Cache::get($this->cacheKey(), collect())
+            ->sortByDesc('score')
+            ->slice(0, 5)
+            ->values();
     }
 
     /**
@@ -31,12 +24,33 @@ class Trending
      *
      * @param Thread $thread
      */
-    public function push($thread)
+    public function push($thread, $increment = 1)
     {
-        Redis::zincrby($this->cacheKey(), 1, json_encode([
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        $trending[$thread->id] = (object) [
+            'score' => $this->score($thread) + $increment,
             'title' => $thread->title,
             'path' => $thread->path(),
-        ]));
+        ];
+
+        Cache::forever($this->cacheKey(), $trending);
+    }
+
+    /**
+     * Get the trending score of the given thread.
+     *
+     * @return int
+     */
+    public function score($thread)
+    {
+        $trending = Cache::get($this->cacheKey(), collect());
+
+        if (!isset($trending[$thread->id])) {
+            return 0;
+        }
+
+        return $trending[$thread->id]->score;
     }
 
     /**
@@ -44,6 +58,16 @@ class Trending
      */
     public function reset()
     {
-        Redis::del($this->cacheKey());
+        return Cache::forget($this->cacheKey());
+    }
+
+    /**
+     * Get the cache key name.
+     *
+     * @return string
+     */
+    private function cacheKey()
+    {
+        return 'trending_threads';
     }
 }
